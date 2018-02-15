391e7cc29c6be05b4ca73139f77c103b2c443cc4
paktus <paktusin@gmail.com>
2018-02-15 21:56:40 +0300

    move bitch!

--- src/index.js
+++ src/index.js
@@ -14,22 +14,44 @@ var Phaser = window.Phaser = require("phaser-ce/build/custom/phaser-split");
  * And render a single sprite so we make sure it works.
  */
 
-var sea;
+var sea, ship, cursors;
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
-    console.log('preload');
     game.load.image('sea', './assets/images/sea.png');
-    game.load.image('ship_1', './assets/images/ship1.png');
-    game.load.image('ship_2', './assets/images/ship2.png');
+    game.load.spritesheet('ship_1', './assets/images/ship1.png', 32, 32);
+    game.load.spritesheet('ship_2', './assets/images/ship2.png', 32, 32);
 }
 
 function create() {
     sea = game.add.tileSprite(0, 0, 800, 600, 'sea');
-
-};
+    ship = game.add.sprite(game.world.width / 2 - 32, game.world.height / 2 - 32, 'ship_1');
+    ship.animations.add('left', [4, 5, 6, 7], 10);
+    ship.animations.add('down', [8, 9, 10, 11], 10);
+    ship.animations.add('up', [12, 13, 14, 15], 10);
+    ship.anchor.set(.5, .5);
+    game.physics.arcade.enable(ship);
+    cursors = game.input.keyboard.createCursorKeys();
+}
 
 function update() {
     sea.tilePosition.y += 2;
+    ship.body.velocity.x = 0;
+    ship.body.velocity.y = 0;
+    if (cursors.left.isDown) {
+        ship.animations.play('left');
+        ship.scale.setTo(1, 1);
+        ship.body.velocity.x = -150;
+    } else if (cursors.right.isDown) {
+        ship.scale.setTo(-1, 1);
+        ship.animations.play('left');
+        ship.body.velocity.x = 150;
+    } else if (cursors.up.isDown) {
+        ship.animations.play('up');
+        ship.body.velocity.y = -150;
+    } else if (cursors.down.isDown) {
+        ship.animations.play('down');
+        ship.body.velocity.y = 150;
+    }
 }
