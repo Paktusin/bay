9b5923e3395fe0a5258cd55956b1080ca86efccd
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-02-16 12:10:37 +0300

    server

--- client/src/index.js
+++ client/src/index.js
@@ -1,57 +1,81 @@
-/**
- * Import Phaser dependencies using `expose-loader`.
- * This makes then available globally and it's something required by Phaser.
- * The order matters since Phaser needs them available before it is imported.
- */
-
 require('expose-loader?PIXI!phaser-ce/build/custom/pixi.js');
 require('expose-loader?p2!phaser-ce/build/custom/p2.js');
 //require('expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js');
 var Phaser = window.Phaser = require("phaser-ce/build/custom/phaser-split");
+var io = require('socket.io-client');
+var Ship = require('./ship');
+require('./actions');
 
-/**
- * Create a new Phaser game instance.
- * And render a single sprite so we make sure it works.
- */
+var client = {};
+var players = {};
+var sea, cursors;
 
-var sea, ship, cursors;
+client.socket = io.connect('http://localhost:8081');
+
+client.askNewPlayer = function (name) {
+    client.socket.emit('newplayer', name);
+};
+client.socket.on('newplayer', function (data) {
+    addNewPlayer(data);
+});
+client.socket.on('action', function (data) {
+    updatePlayer(data);
+});
+client.socket.on('remove', function (id) {
+    removePlayer(id);
+});
+client.action = function (action) {
+    client.socket.emit('action', action);
+};
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
     game.load.image('sea', './assets/images/sea.png');
     game.load.spritesheet('ship_1', './assets/images/ship1.png', 32, 32);
-    game.load.spritesheet('ship_2', './assets/images/ship2.png', 32, 32);
 }
 
 function create() {
     sea = game.add.tileSprite(0, 0, 800, 600, 'sea');
-    ship = game.add.sprite(game.world.width / 2 - 32, game.world.height / 2 - 32, 'ship_1');
-    ship.animations.add('left', [4, 5, 6, 7], 10);
-    ship.animations.add('down', [8, 9, 10, 11], 10);
-    ship.animations.add('up', [12, 13, 14, 15], 10);
-    ship.anchor.set(.5, .5);
-    game.physics.arcade.enable(ship);
     cursors = game.input.keyboard.createCursorKeys();
+    client.askNewPlayer({name: 'Name' + Math.random()})
 }
 
 function update() {
+    Object.keys(players).forEach(function (id) {
+        players[id].action()
+    });
     sea.tilePosition.y += .1;
-    ship.body.velocity.x = 0;
-    ship.body.velocity.y = 0;
     if (cursors.left.isDown) {
+        client.move('left');
+        ship
         ship.animations.play('left');
         ship.scale.setTo(1, 1);
         ship.body.velocity.x = -150;
     } else if (cursors.right.isDown) {
+        client.action(ACTION_LEFT);
         ship.scale.setTo(-1, 1);
         ship.animations.play('left');
         ship.body.velocity.x = 150;
     } else if (cursors.up.isDown) {
+        client.move('up');
         ship.animations.play('up');
         ship.body.velocity.y = -150;
     } else if (cursors.down.isDown) {
+        client.move('down');
         ship.animations.play('down');
         ship.body.velocity.y = 150;
     }
+}
+
+function addNewPlayer(data) {
+    players[data.id] = new Ship()
+}
+
+function updatePlayer(data) {
+
+}
+
+function removePlayer(id) {
+    delete players[id];
 }
--- server/server.js
+++ server/server.js
@@ -2,3 +2,49 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+
+app.use('/assets', express.static(__dirname + '/node_modules'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/status', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(
+        getAllPlayers()
+    ));
+});
+server.listen(8081, function () { // Listens to port 8081
+    console.log('Listening on ' + server.address().port);
+});
+server.lastPlayderID = 0;
+io.on('connection', function (socket) {
+    socket.on('newplayer', function (data) {
+        console.log('client call newplayer ' + data.name);
+        socket.player = {
+            id: server.lastPlayderID++,
+            name: data.name,
+            x: randomInt(100, 700),
+            y: randomInt(100, 600)
+        };
+        socket.emit('allplayers', getAllPlayers());
+        socket.broadcast.emit('newplayer', socket.player);
+    });
+    socket.on('disconnect', function () {
+        if(socket.player){
+            console.log('client disconnected ' + socket.player.name);
+            io.emit('remove', socket.player.id);
+        }
+    });
+});
+function getAllPlayers() {
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function (socketID) {
+        var player = io.sockets.connected[socketID].player;
+        if (player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
