482ec5eebf536e136995288973346907b3bc10b3
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-12-12 16:51:40 +0300

    update

--- client3D/app.js
+++ client3D/app.js
@@ -3,11 +3,11 @@ window.addEventListener('DOMContentLoaded', function () {
     var engine = new BABYLON.Engine(canvas, true);
     var createScene = function () {
         var scene = new BABYLON.Scene(engine);
-        var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
+        var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 10, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
         // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+        var skybox = BABYLON.Mesh.CreateBox("skyBox", 10000, scene);
         var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/textures/TropicalSunnyDay", scene);
@@ -24,8 +24,8 @@ window.addEventListener('DOMContentLoaded', function () {
         var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
         groundMaterial.diffuseTexture = groundTexture;
 
-        var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
-        ground.position.y = -64;
+        var ground = BABYLON.Mesh.CreateGround("ground", 1000, 1000, 32, scene, false);
+        ground.position.y = -100;
         ground.material = groundMaterial;
 
         // Water
@@ -34,10 +34,10 @@ window.addEventListener('DOMContentLoaded', function () {
         water.backFaceCulling = true;
         water.bumpTexture = new BABYLON.Texture("assets/textures/waterbump.png", scene);
         water.windForce = 5;
-        water.waveHeight = 0.5;
-        water.bumpHeight = 0.1;
-        water.waveLength = 0.1;
-        water.colorBlendFactor = 0;
+        water.waveHeight = .1;
+        water.bumpHeight = .1;
+        water.waveLength = .1;
+        water.colorBlendFactor = .12;
         water.addToRenderList(skybox);
         water.addToRenderList(ground);
         waterMesh.material = water;
